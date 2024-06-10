229ba6309bfe265e9dacfbfad7a3ab7eeab8dd90
behzad azadi <behzad.azadi2693@gmail.com>
2024-06-10 01:21:45 -0700

    update cors header and configurations

--- config/settings.py
+++ config/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'drf_spectacular',
     'rest_framework',
     'accounts',
@@ -52,6 +53,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -159,7 +161,10 @@ REST_FRAMEWORK = {
 if DEBUG:
      CORS_ORIGIN_ALLOW_ALL = True
 else:
-    CORS_ALLOWED_ORIGINS = str(config('CORS_ALLOWED_ORIGINS', cast=str)).split(',')
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        rf"^https:\/\/(\w+\.)?{config('CORS_ALLOWED_ORIGINS')}$",
+        rf"^http:\/\/(\w+\.)?{config('CORS_ALLOWED_ORIGINS')}$"
+        ]
 
 CORS_ALLOW_METHODS = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS']
 CORS_ALLOW_HEADERS = ['Content-Type', 'Authorization']
