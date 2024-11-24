e5fe6fc4d1ebde98dc007b8f13528bc72929f6ac
behzad-azadi2693 <behzad-azadi2693@gmail.com>
2024-11-24 15:54:57 +0330

    fit(account): transfer from session to redis

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -14,7 +14,7 @@ def valid_phone_email(username):
     email = re.match(r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}', username)
     return phone, email
 
-
+#add redis
 class SignupSerializer(serializers.Serializer):
     username = serializers.CharField(required=True)
     password = serializers.CharField(required=True)
@@ -45,6 +45,15 @@ class SignupSerializer(serializers.Serializer):
             user.create_phone()
         if email:
             user.create_email()
+
+        #redis username otp=6N time=10
+        '''
+        if phone:
+            otp_phone_sender(attrs['otp'], phone)
+        if email:
+            otp_email_sender(attrs['otp'], email)
+        '''
+
         return validated_data['username']
 
 
@@ -64,15 +73,8 @@ class OTPRequestSeriailizer(serializers.Serializer):
             raise serializers.ValidationError({'message':'username not exsits'}, code=404)
         if user.check_enable() == 404:
             raise serializers.ValidationError({'message':'user not available calling with admin'}, code=400)
-
-        '''
-        if phone:
-            otp_phone_sender(attrs['otp'], phone)
-        if email:
-            otp_email_sender(attrs['otp'], email)
-        '''
         return attrs
-
+    
 
 class OTPSigninSerializer(serializers.Serializer):
     username = serializers.CharField(required=True)
@@ -106,69 +108,24 @@ class OTPSigninSerializer(serializers.Serializer):
         if user.check_enable() == 404:
             raise serializers.ValidationError({'message':'user not available calling with admin'}, code=400)
         return attrs
-
-
-class OTPPasswordChangeVerifySerializer(OTPSigninSerializer):
-    username = serializers.CharField(required=True)
-    otp = serializers.IntegerField(required=True)
-
-    def validate(self, attrs):
-        otp, time, count, username = self.context['otp'], self.context['time'], self.context['count'], self.context['username']
-        if None in [time, username, otp, count]:
-            raise serializers.ValidationError({'message':'you dont access for this page'}, code=404)
-    
-        phone, email = valid_phone_email(attrs['username'])
-        time_delta, now_time = make_naive(datetime.fromisoformat(time) + timedelta(minutes=5)), make_naive(datetime.now().astimezone(pytz.timezone('Asia/Tehran')))
-        user = UserKeyCloak()
-        user.username = attrs['username']
-        self.context['request'].session['OTP_ITS_COUNT'] = count + 1
-
-        if user.check_connect() == 500:
-            raise serializers.ValidationError({'message':'server not found'}, code=500)
-        if not phone and not email:
-            raise serializers.ValidationError({'message':'username is not correct'}, code=400)
-        if count > 5:
-            self.context['request'].session.flush()
-            raise serializers.ValidationError({'message':'otp is expired'}, code=410)
-        if time_delta < now_time:
-            self.context['request'].session.flush()
-            raise serializers.ValidationError({'message':'timing is expired for otp'}, code=410)
-        if not bcrypt.checkpw(f"{attrs['otp']}+{settings.VALUE_HASH}".encode(), otp.encode()):
-            raise serializers.ValidationError({'message':'otp or username is not   correct'}, code=400)
-        if user.check_email_verify() == 404:
-            raise serializers.ValidationError({'message':'please first verified username'}, code=400)
-        if user.check_enable() == 404:
-            raise serializers.ValidationError({'message':'user not available calling with admin'}, code=400)
-        return attrs
     
 
+#add redis
 class OTPSingnupVerifySerializer(serializers.Serializer):
     username = serializers.CharField(required=True)
     otp = serializers.IntegerField(required=True)
 
     def validate(self, attrs):
-        otp, time, count, username = self.context['otp'], self.context['time'], self.context['count'], self.context['username']
-        if None in [time, username, otp, count]:
-            raise serializers.ValidationError({'message':'you dont access for this page'}, code=404)
-    
+
         phone, email = valid_phone_email(attrs['username'])
-        time_delta, now_time = make_naive(datetime.fromisoformat(time) + timedelta(minutes=5)), make_naive(datetime.now().astimezone(pytz.timezone('Asia/Tehran')))
         user = UserKeyCloak()
         user.username = attrs['username']
-        self.context['request'].session['OTP_ITS_COUNT'] = count + 1
-
+        # redis check otp username time
         if user.check_connect() == 500:
             raise serializers.ValidationError({'message':'server not found'}, code=500)
         if not phone and not email:
             raise serializers.ValidationError({'message':'username is not correct'}, code=400)
-        if count > 5:
-            self.context['request'].session.flush()
-            raise serializers.ValidationError({'message':'otp is expired'}, code=410)
-        if time_delta < now_time:
-            self.context['request'].session.flush()
-            raise serializers.ValidationError({'message':'timing is expired for otp'}, code=410)
-        if not bcrypt.checkpw(f"{attrs['otp']}+{settings.VALUE_HASH}".encode(), otp.encode()):
-            raise serializers.ValidationError({'message':'otp or username is not   correct'}, code=400)
+
         if user.check_email_verify() == 200:
             raise serializers.ValidationError({'message':'usr befor exsits'}, code=400)
         if user.check_enable() == 404:
@@ -187,40 +144,36 @@ class PasswordChangeSerializer(serializers.Serializer):
     password_confierm = serializers.CharField(required=True)
 
     def validate(self, attrs):
-        time, username, change =  self.context['time'], self.context['username'], self.context['change']
-        if None in [time, username, change]:
-            raise serializers.ValidationError({'message':'you dont access for this page'}, code=404)
-
-        phone, email = valid_phone_email(username)
-        time_delta, now_time = make_naive(datetime.fromisoformat(time) + timedelta(minutes=5)), make_naive(datetime.now().astimezone(pytz.timezone('Asia/Tehran')))
         user = UserKeyCloak()
-        user.username = username
-
+        access_token = self.context['request'].META.get('HTTP_AUTHORIZATION', '').split('Bearer ')[-1]
+        user_token = TokenKeycloak()
+        user_token.token = access_token
+        info = user_token.decode_token
+        user = UserKeyCloak()
+        user.username = info['username']
+        
         if user.check_connect() == 500:
             raise serializers.ValidationError({'message':'server not found'}, code=500)
-        if not phone and not email:
-            raise serializers.ValidationError({'message':'username is not correct'}, code=400)
-        if not change:
-            raise serializers.ValidationError({'message':'you can not change password'}, code=400)
-        if time_delta < now_time:
-            raise serializers.ValidationError({'message':'timing is expired for otp'}, code=410)
         if user.check_email_verify() == 404:
             raise serializers.ValidationError({'message':'please first verified username'}, code=400)
         if user.check_enable() == 404:
             raise serializers.ValidationError({'message':'user not available calling with admin'}, code=400)
-        if attrs['password'] != attrs['password_confierm']:
-            raise serializers.ValidationError({'message': 'password not mached'}, code=401)
+        if attrs['password'] != attrs['password_confirm']:
+            raise serializers.ValidationError({'password confirm':'password not matched'}, code=400)
         return attrs
 
-    def create(self, validated_data):
-        user = UserKeyCloak()
-        user.password = validated_data['password']
-        user.username = self.context['username']
-        user.change_password()
-        token = TokenKeycloak()
-        token.username = self.context['username']
-        token.signout()
-        return validated_data['password']
+    def put(self, validated_data):
+        access_token = self.context['request'].META.get('HTTP_AUTHORIZATION', '').split('Bearer ')[-1]
+        user_token = TokenKeycloak()
+        user_token.token = access_token
+        info = user_token.decode_token
+
+        user_keycloak = UserKeyCloak()
+        user_keycloak.username = info['username']
+        user_keycloak.password = validated_data['password']
+        user_keycloak.change_password()
+        
+        return validated_data
 
 
 class PasswordSinginSerializer(serializers.Serializer):
